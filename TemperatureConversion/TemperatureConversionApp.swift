15670ff1b707937d6a4511efb4e08ae1916b927d
Guriqbal Singh Amroke <amroke@icloud.com>
2026-02-05 13:23:44 +0100

    Initial Commit

---
+++ TemperatureConversion/TemperatureConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversionApp.swift
+//  TemperatureConversion
+//
+//  Created by Guriqbal Singh Amroke on 05.02.26.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
